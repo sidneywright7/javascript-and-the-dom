614d50552311ede03798937d6087e2af4dcaaf19
sidneywright7 <sidneywright7@gmail.com>
2026-01-06 13:01:16 -0600

    exercise03

---
+++ module 1/exercise03/tic-tac-toe.js
@@ -0,0 +1,36 @@
+function getMove() {
+    const value = Math.random();
+    
+    return value < 0.33 ? 'X' :
+           value < 0.66 ? 'O' : null;
+}
+
+const board = new Array(20).fill(null).map(
+    () => new Array(20).fill(null).map(getMove)
+);
+
+const container = document.querySelector('.js-container');
+
+function renderRow(rowData){
+    let rowHtml = '<tr class="tic-tac-toe-row">';
+
+    for (let cell of rowData) {
+        rowHtml += `<td class= "tic-tac-toe-cell>${cell}</td>`;
+    }
+
+    rowHtml += '</tr>';
+    return rowHtml;
+} 
+
+function renderBoard(board) {
+    let html = '<table class="tic-tac-toe-board"><tbody>';
+
+    for (let row of board) {
+        html += renderRow(row);
+    }
+        
+    html += '</tbody></table>';
+    return html;    
+}   
+
+let html = renderBoard(board);
