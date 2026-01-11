76ebedf8fb34e90a137e606f4b3629f7ed76b261
sidneywright7 <sidneywright7@gmail.com>
2026-01-10 20:26:41 -0600

    exercise 1

---
+++ module 2/exercise01/counter.js
@@ -0,0 +1,24 @@
+let counterValue = 0;
+
+function renderCounterValue() {
+    const container = document.querySelector('.js-counter');
+    container.textContent = counterValue;
+}
+
+function countCallback() {
+    counterValue += 1;
+    renderCounterValue();
+}   
+
+function resetCallback() {
+    counterValue = 0;
+    renderCounterValue();
+}
+
+const countButton = document.querySelector('.js-1up-button');
+const resetButton = document.querySelector('.js-reset-button');
+
+countButton.addEventListener('click', countCallback);
+resetButton.addEventListener('click', resetCallback);
+
+renderCounterValue();
